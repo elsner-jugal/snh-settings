d838d23aeca9447cecdd586a78b181f236809240
seansan <sheukels+github@gmail.com>
2014-08-22 10:39:07 +0200

    Add initial

---
+++ app/code/community/SNH/SkipStep1/Helper/Data.php
@@ -0,0 +1,8 @@
+<?php
+class SNH_SkipStep1_Helper_Data extends Mage_Core_Helper_Data
+{
+    public function isSkipEnabled()
+    {
+        return Mage::getStoreConfig('snh_settings/messages/skipstep1_enabled');
+    }
+}
