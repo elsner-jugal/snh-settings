815a928a9461814b34af3cc75195f3ea03d25ae9
seansan <sheukels+github@gmail.com>
2014-08-25 20:28:29 +0200

    Added new submodule SNH_CategoryParentUrl
    Jugal please check code - not workign yet

---
+++ app/code/local/SNH/CategoryParentUrl/Model/Url.php
@@ -0,0 +1,65 @@
+<?php
+
+class SNH_CategoryParentUrl_Catalog_Model_Product_Url extends Mage_Catalog_Model_Url
+{
+
+	/**
+     * Get unique category request path
+     *
+     * @param Varien_Object $category
+     * @param string $parentPath
+     * @return string
+     */
+    public function getCategoryRequestPath($category, $parentPath)
+    {
+        $storeId = $category->getStoreId();
+        $idPath  = $this->generatePath('id', null, $category);
+        $suffix  = $this->getCategoryUrlSuffix($storeId);
+
+        if (isset($this->_rewrites[$idPath])) {
+            $this->_rewrite = $this->_rewrites[$idPath];
+            $existingRequestPath = $this->_rewrites[$idPath]->getRequestPath();
+        }
+
+        if ($category->getUrlKey() == '') {
+            $urlKey = $this->getCategoryModel()->formatUrlKey($category->getName());
+        }
+        else {
+            $urlKey = $this->getCategoryModel()->formatUrlKey($category->getUrlKey());
+        }
+
+        $categoryUrlSuffix = $this->getCategoryUrlSuffix($category->getStoreId());
+        
+		/* SNH custom code Magento 1.8.1 verify before upgrade */
+		$configValue = Mage::getStoreConfig('catalog/seo/category_use_parentcategory',Mage::app()->getStore());
+
+		if (null === $configValue || $configValue) {
+		
+			if (null === $parentPath) {
+				$parentPath = $this->getResource()->getCategoryParentPath($category);
+			}
+			elseif ($parentPath == '/') {
+				$parentPath = '';
+			}
+		}
+		
+		$parentPath = Mage::helper('catalog/category')->getCategoryUrlPath($parentPath,
+                                                                           true, $category->getStoreId());
+
+        $requestPath = $parentPath . $urlKey . $categoryUrlSuffix;
+        if (isset($existingRequestPath) && $existingRequestPath == $requestPath . $suffix) {
+            return $existingRequestPath;
+        }
+
+        if ($this->_deleteOldTargetPath($requestPath, $idPath, $storeId)) {
+            return $requestPath;
+        }
+
+        return $this->getUnusedPath($category->getStoreId(), $requestPath,
+                                    $this->generatePath('id', null, $category)
+        );
+    }
+
+
+}
+		
