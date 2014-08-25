df2424db0e4179b3488d84972833761e999c1543
Jugal Patel <jugal@xhtmljunkies.com>
2014-08-25 19:31:15 +0530

    Removed old module files

--- app/code/community/SNH/SkipStep1/Helper/Data.php
+++
@@ -1,8 +0,0 @@
-<?php
-class SNH_SkipStep1_Helper_Data extends Mage_Core_Helper_Data
-{
-    public function isSkipEnabled()
-    {
-        return Mage::getStoreConfig('snh_settings/messages/skipstep1_enabled');
-    }
-}
